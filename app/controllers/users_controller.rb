bf05db8e4ca0851e1ce8b003a27c97ce0f968d6c
saipratap99 <122005067@sastra.ac.in>
2020-08-21 14:11:11 +0530

    Added redirection and validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,20 @@ class UsersController < ApplicationController
   def create
     regno = params[:regno]
     name = params[:name]
+    user = User.where("regno = ?", regno).exists? ?
+      User.where("regno = ?", regno).first :
+      User.new(name: name, regno: regno.to_i)
     if (regno.length == 9) && (name.length > 3)
-      user = User.create(name: name, regno: regno.to_i)
-      @current_user = user
-      puts(@current_user.id)
-      session[:current_user_id] = user.id
-      redirect_to(questions_path, notice: "Your'e signed in successfully!")
+      if user.save
+        @current_user = user
+        session[:current_user_id] = user.id
+        redirect_to(questions_path, notice: "Your'e signed in successfully!")
+      else
+        flash[:error] = user.errors.full_messages
+        redirect_to root_path
+      end
     else
-      redirect_to(root_path, alert: "Invalid Name or regno")
+      redirect_to(root_path, alert: "Invalid Name or Registration number")
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :user_questions
+  validates :name, presence: true, length: { in: 3..50 }
+  validates :regno, presence: true, uniqueness: true
 end
