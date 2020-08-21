0f1fb9efe2424ac6b9cfcc02fabe07736a19ec93
saipratap99 <122005067@sastra.ac.in>
2020-08-21 21:32:09 +0530

    Added js file from controller

--- app/controllers/user_questions_controller.rb
+++ app/controllers/user_questions_controller.rb
@@ -2,4 +2,16 @@ class UserQuestionsController < ApplicationController
   def index
     @questions = @current_user.user_questions
   end
+
+  def save_score
+    id = params[:id]
+    score = params[:score]
+    @ques = UserQuestion.find(id)
+    @ques.scored = score.to_i
+    @ques.save
+    respond_to do |format|
+      format.html { redirect_to("/user_questions/index", notice: "Response submited!") }
+      format.js
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  post "user_questions/score/:id", to: "user_questions#save_score", as: :save_score
   get "user_questions/index", to: "user_questions#index", as: :questions
   root to: "users#new"
   resources :users
-  resources :user_questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
